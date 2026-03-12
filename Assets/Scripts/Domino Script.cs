d996674d3b258bbdf869c2a864a29dbb8b6834cf
Tariq-Sekhri <tariqsekhri@gmail.com>
2026-03-11 21:19:38 -0400

    changes

--- Assets/Scripts/Domino Script.cs
+++ Assets/Scripts/Domino Script.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 
-public enum directions
+
+
+public class DominoScript : MonoBehaviour
+{
+
+    public enum Directions
 {
     right,
     left,
     up,
     down,
 }
-
-public class DominoScript : MonoBehaviour
-{
     public GameObject player;
     public GameObject side1;
     public GameObject side2;
@@ -23,8 +26,7 @@ public class DominoScript : MonoBehaviour
     public float side1Value;
     public float side2Value;
 
-    public float UpValue;
-    public float DownValue;
+
 
     public float direction;
 
@@ -46,23 +48,15 @@ public class DominoScript : MonoBehaviour
 
     void Update()
     {
-        if (side1.transform.position.y > side2.transform.position.y) 
-        {
-            UpValue = side1Value;
-            DownValue = side2Value;
-        } else
-        {
-            UpValue = side2Value;
-            DownValue = side1Value;
-        }
+   
     }
 
     void OnMouseDown()
     {
-       //GameObject player = GetComponent<Player>();
+      
        if (player.GetComponent<Player>().dominos.Contains(this.gameObject))
        {
-            Debug.Log("clicked: " + this);
+            Debug.Log("dom clicked: " + this);
             ClearFakeDoms();
 
             gameManager.CheckValidMoves(this);
@@ -83,6 +77,29 @@ public class DominoScript : MonoBehaviour
         }
     }
 
-   
+    public float getUpValue()
+    {
+        if (side1.transform.position.y > side2.transform.position.y) {
+            return side1Value;
+        } else{
+            return side2Value;
+        }
+    }
 
+    public float getDownValue()
+    {
+        if (side1.transform.position.y > side2.transform.position.y)
+        {
+            return side2Value;
+        }
+        else
+        {
+            return side1Value;
+        }
+    }
+    
+    public override string ToString()
+    {
+        return $"{side1Value} | {side2Value}";
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,10 @@ using UnityEngine;
 using System;
 using System.Numerics;
 using System.Collections.Generic;
-public enum Directions
-{
-    right,
-    left,
-    up,
-    down,
-}
+using Directions = DominoScript.Directions;
 
 public class GameManager : MonoBehaviour
 {
-    public System.Numerics.Vector2 Domino;
     public List<System.Numerics.Vector2> dominoVectorsList;
 
     public GameObject DominoObject;
@@ -21,12 +14,12 @@ public class GameManager : MonoBehaviour
 
     public int Team1Score;
     public int Team2Score;
-
-    public bool roundStart;
     public UnityEngine.Vector3 offset;
     public int playerWhosTurnItIsIndex;
     public GameObject RootDomino;
     public GameObject[] Ends;
+    string lastEndsState = "";
+
 
     void Start()
     {
@@ -34,7 +27,6 @@ public class GameManager : MonoBehaviour
         RoundStart();
         Display();
     }
-    string lastEndsState = "";
 
     void Update()
     {
@@ -52,50 +44,7 @@ public class GameManager : MonoBehaviour
 
         PrintEndsIfChanged();
     }
-    /* public float GetBoardEndTotal(Directions dir)
-     {
-         float total = 0;
-         HashSet<GameObject> counted = new HashSet<GameObject>();
-
-         for (int i = 0; i < Ends.Length; i++)
-         {
-             GameObject endObj = Ends[i];
-
-             if (endObj == null)
-                 continue;
-
-             if (counted.Contains(endObj))
-                 continue;
-
-             counted.Add(endObj);
-
-             var domino = endObj.GetComponent<DominoScript>();
-
-             int occurrences = 0;
-             for (int j = 0; j < Ends.Length; j++)
-             {
-                 if (Ends[j] == endObj)
-                     occurrences++;
-             }
-
-             bool isDouble = domino.side1Value == domino.side2Value;
-
-             // If a double has exactly two connections, it's closed and shouldn't count
-             if (isDouble && occurrences == 2)
-                 continue;
-
-             if (endObj == RootDomino)
-             {
-                 total += domino.side1Value + domino.side2Value;
-             }
-             else
-             {
-                 total += GetEndValue(endObj, dir);
-             }
-         }
-         return total;
-     }
-    */
+   
     public float GetBoardEndTotal()
     {
         if (RootDomino == null)
@@ -178,10 +127,10 @@ public class GameManager : MonoBehaviour
             case 0:
             case 90:
             case 270:
-                return end.UpValue;
+                return end.getUpValue();
 
             case 180:
-                return end.DownValue;
+                return end.getDownValue();
 
             default:
                 Debug.LogError("Invalid direction");
@@ -199,10 +148,10 @@ public class GameManager : MonoBehaviour
             case 0:
             case 90:
             case 270:
-                return end.DownValue;
+                return end.getDownValue();
 
             case 180:
-                return end.UpValue;
+                return end.getUpValue();
 
             default:
                 Debug.LogError("Invalid direction");
@@ -219,9 +168,9 @@ public class GameManager : MonoBehaviour
             case 0:
             case 180:
             case 270:
-                return end.UpValue;
+                return end.getUpValue();
             case 90:
-                return end.DownValue;
+                return end.getDownValue();
 
             default:
                 Debug.LogError("Invalid direction");
@@ -238,10 +187,10 @@ public class GameManager : MonoBehaviour
             case 0:
             case 180:
             case 90:
-                return end.UpValue;
+                return end.getUpValue();
 
             case 270:
-                return end.DownValue;
+                return end.getDownValue();
 
             default:
                 Debug.LogError("Invalid direction");
@@ -251,12 +200,11 @@ public class GameManager : MonoBehaviour
 
     void PrintEndsIfChanged()
     {
-        string currentState =
-            $"up: {Ends[2].GetComponent<DominoScript>().UpValue} | {Ends[2].GetComponent<DominoScript>().DownValue}, " +
-            $"down: {Ends[3].GetComponent<DominoScript>().UpValue} | {Ends[3].GetComponent<DominoScript>().DownValue}, " +
-            $"left: {Ends[1].GetComponent<DominoScript>().UpValue} | {Ends[1].GetComponent<DominoScript>().DownValue}, " +
-            $"right: {Ends[0].GetComponent<DominoScript>().UpValue} | {Ends[0].GetComponent<DominoScript>().DownValue}";
-
+string currentState =
+    $"up: {Ends[2].GetComponent<DominoScript>().getUpValue()} | {Ends[2].GetComponent<DominoScript>().getDownValue()}, " +
+    $"down: {Ends[3].GetComponent<DominoScript>().getUpValue()} | {Ends[3].GetComponent<DominoScript>().getDownValue()}, " +
+    $"left: {Ends[1].GetComponent<DominoScript>().getUpValue()} | {Ends[1].GetComponent<DominoScript>().getDownValue()}, " +
+    $"right: {Ends[0].GetComponent<DominoScript>().getUpValue()} | {Ends[0].GetComponent<DominoScript>().getDownValue()}";
         if (currentState != lastEndsState)
         {
             lastEndsState = currentState;
@@ -318,28 +266,15 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < howManyDominos; i++)
         {
-            // Safety check in case we run out of dominos
             if (dominoVectorsList.Count == 0) break;
-
             UnityEngine.Vector3 dominoPosition = player.transform.position + offset * i;
-
             GameObject newDomino = Instantiate(DominoObject, dominoPosition, transform.rotation);
             newDomino.transform.SetParent(player.transform, true);
             player.GetComponent<Player>().dominos.Add(newDomino);
-
             var dominoScript = newDomino.GetComponent<DominoScript>();
-
-            // --- MOVED LOGIC HERE ---
-            // 1. Pick random index (Note: Random.Range int is exclusive max, so use Count, not Count-1)
             int randomIndex = UnityEngine.Random.Range(0, dominoVectorsList.Count);
-
-            // 2. Get data
             System.Numerics.Vector2 randomDomino = dominoVectorsList[randomIndex];
-
-            // 3. Remove from list
             dominoVectorsList.RemoveAt(randomIndex);
-
-            // 4. Initialize the script immediately so values are ready for checking
             dominoScript.Setup(randomDomino.X, randomDomino.Y, player);
         }
     }
@@ -354,27 +289,21 @@ public class GameManager : MonoBehaviour
 
         bool rootFound = false;
 
-        // Find the heaviest double
-        // Note: The original logic looked for *any* double. Usually, domino rules look for the highest double (6|6).
-        // This preserves your original logic of finding the first one encountered in the loops.
         for (int i = 0; i < Players.Length; i++)
         {
             var player = Players[i].GetComponent<Player>();
 
-            // Iterate backwards so we can remove safely
             for (int j = player.dominos.Count - 1; j >= 0; j--)
             {
                 var domino = player.dominos[j];
                 var cur = domino.GetComponent<DominoScript>();
 
-                // Compare numeric values now (string comparison works too but floats are safer)
                 float v1 = cur.side1Value;
                 float v2 = cur.side2Value;
 
-
-                if (v1 == v2) // It's a double
+                bool is_double = v1 == v2;
+                if (is_double) // It's a double
                 {
-                    // Logic to see if this is the double we want (currently accepts the first one found)
                     player.dominos.RemoveAt(j);
                     RootDomino = domino;
                     RootDomino.transform.SetParent(null, false);
@@ -410,134 +339,47 @@ public class GameManager : MonoBehaviour
     public void CheckValidMoves(DominoScript domino)
     {
 
-        //var Domino = DominoObject.GetComponent<DominoScript>();
-        //var v1 = Domino.side1Value;
-        //var v2 = Domino.side2Value;
         float v1 = domino.side1Value;
         float v2 = domino.side2Value;
+
+
         var rightEnd = Ends[0].GetComponent<DominoScript>();
-        float rval = -1;
         int Rdir = Mathf.RoundToInt(rightEnd.direction);
-        switch (Rdir)
-        {
-            case 0:
-            case 90:
-            case 270:
-                rval = rightEnd.side2Value;
-                break;
-            case 180:
-                rval = rightEnd.side1Value;
-                break;
-            default:
-                Debug.LogError("ahhhh");
-                break;
-        }
-        //makeFakeDom(v1, v2, rightEnd, rval, false, new UnityEngine.Vector3(2f, 0f,0f));
+        float rval = (Rdir == 180) ? rightEnd.side1Value : rightEnd.side2Value;
         makeFakeDom(v1, v2, rightEnd, 0, rval, new UnityEngine.Vector3(2f, 0f, 0f), domino);
 
         var LeftEnd = Ends[1].GetComponent<DominoScript>();
-
-        float lval = -1;
         int Ldir = Mathf.RoundToInt(LeftEnd.direction);
-        switch (Ldir)
-        {
-            case 0:
-            case 90:
-            case 270:
-                lval = LeftEnd.side1Value;
-                break;
-            case 180:
-                lval = LeftEnd.side2Value;
-                break;
-            default:
-                Debug.LogError("ahhhh");
-                break;
-        }
-        //makeFakeDom(v1, v2, LeftEnd, lval, false, new UnityEngine.Vector3(-2f, 0f, 0f));
+        float lval = (Ldir == 180) ? LeftEnd.side2Value : LeftEnd.side1Value;
         makeFakeDom(v1, v2, LeftEnd, 1, lval, new UnityEngine.Vector3(-2f, 0f, 0f), domino);
 
         var UpEnd = Ends[2].GetComponent<DominoScript>();
-        float Uval = -1;
         int Udir = Mathf.RoundToInt(UpEnd.direction);
-        switch (Udir)
-        {
-            case 0:
-            case 180:
-            case 270:
-                Uval = UpEnd.side2Value;
-                break;
-            case 90:
-                Uval = UpEnd.side1Value;
-                break;
-            default:
-                Debug.LogError("ahhhh");
-                break;
-        }
-        //makeFakeDom(v1, v2, UpEnd, Uval, true, new UnityEngine.Vector3(0f, 2f, 0f));
+        float Uval = (Udir == 90) ? UpEnd.side1Value : UpEnd.side2Value;
         makeFakeDom(v1, v2, UpEnd, 2, Uval, new UnityEngine.Vector3(0f, 2f, 0f), domino);
 
         var DownEnd = Ends[3].GetComponent<DominoScript>();
-        float Dval = -1;
         int Ddir = Mathf.RoundToInt(DownEnd.direction);
-        switch (Ddir)
-        {
-            case 0:
-            case 180:
-            case 90:
-                Dval = DownEnd.side2Value;
-                break;
-            case 270:
-                Dval = DownEnd.side1Value;
-                break;
-            default:
-                Debug.LogError("ahhhh");
-                break;
-        }
-        //makeFakeDom(v1, v2, DownEnd, Dval, true, new UnityEngine.Vector3(0f, -2f, 0f));
+        float Dval = (Ddir == 270) ? DownEnd.side1Value : DownEnd.side2Value;
         makeFakeDom(v1, v2, DownEnd, 3, Dval, new UnityEngine.Vector3(0f, -2f, 0f), domino);
-
-
-
     }
 
     public float getOffset(DominoScript end)
     {
         int dir = Mathf.RoundToInt(end.direction);
-        switch (dir)
-        {
-            case 0:
-            case 180:
-                return 2f;
-            case 90:
-            case 270:
-                return 0.5f;
-            default:
-                return -1f;
-        }
+        if (dir == 0 || dir == 180) return 2f;
+        if (dir == 90 || dir == 270) return 0.5f;
+        return -1f;
     }
 
     public void makeFakeDom(float v1, float v2, DominoScript end, int index, float val, UnityEngine.Vector3 offsetDir, DominoScript domino)
     {
-        if (val != v1 && val != v2)
-            return;
+        if (val != v1 && val != v2) { return; }
 
         UnityEngine.Vector3 pos = end.transform.position;
-         //UnityEngine.Vector3 FakedomPosition = pos + offsetDir;
 
          float baseRotation = 0f;
-        /*
-         if (offsetDir == new UnityEngine.Vector3(2f, 0f, 0f))        
-             baseRotation = 0f;
-
-         else if (offsetDir == new UnityEngine.Vector3(-2f, 0f, 0f))  
-             baseRotation = 180f;
-
-         else if (offsetDir == new UnityEngine.Vector3(0f, 2f, 0f))   
-             baseRotation = 90f;
-
-         else if (offsetDir == new UnityEngine.Vector3(0f, -2f, 0f))  
-             baseRotation = 270f;
-        */
+      
         if (offsetDir.x > 0)
             baseRotation = 0f;
 
@@ -549,30 +391,8 @@ public class GameManager : MonoBehaviour
 
         else if (offsetDir.y < 0)
             baseRotation = 270f;
-        /*
-       if (val == v2)
-           baseRotation += 180f;
-
-       if (v1 == v2)
-           baseRotation += 90f;
       
-
-        // Determine which side connects to the board
-        bool side1Matches = (v1 == val);
-        bool side2Matches = (v2 == val);
-
-        // rotate so the matching side touches the board
-        if (side1Matches && !side2Matches)
-        {
-            baseRotation += 180f;
-        }
-        else if (side1Matches && side2Matches)
-        {
-            // double
-            baseRotation += 90f;
-        }
-        */
-        bool isDouble = (v1 == v2);
+        bool isDouble = v1 == v2;
 
         if (!isDouble)
         {
@@ -587,33 +407,25 @@ public class GameManager : MonoBehaviour
         }
         UnityEngine.Quaternion rotation = UnityEngine.Quaternion.Euler(0f, 0f, baseRotation);
          
-        // Determine placement orientation
         bool placingVertical = Mathf.Abs(offsetDir.y) > 0f;
 
-        // Determine if the end domino is vertical
         bool endIsVertical = Mathf.Abs(end.transform.eulerAngles.z % 180f) > 1f;
 
-        // Compute half-lengths along placement direction
         float endHalf = placingVertical
-        ? (endIsVertical ? 1f : 0.5f) // vertical placement uses end's height/2
-        : (endIsVertical ? 0.5f : 1f); // horizontal placement uses end's width/2
+        ? (endIsVertical ? 1f : 0.5f) 
+        : (endIsVertical ? 0.5f : 1f); 
 
-        float newHalf = placingVertical ? 1f : 1f; // new domino length in placement direction is always 2 units total, so half = 1
+        float newHalf = placingVertical ? 1f : 1f; 
         
-        // Compute spacing
         float spacing = endHalf + newHalf;
 
-        // Final offset from end domino center
         UnityEngine.Vector3 finalOffset = new UnityEngine.Vector3(
         Mathf.Sign(offsetDir.x) * (placingVertical ? 0f : spacing),
         Mathf.Sign(offsetDir.y) * (placingVertical ? spacing : 0f),
         0f
         );
 
-        // Position for the new domino
         UnityEngine.Vector3 FakedomPosition = end.transform.position + finalOffset;
-
-
         GameObject fakeDomObj = Instantiate(FakeObject, FakedomPosition, rotation);
         fakeDomObj.GetComponent<FakeDom>().setup(v1, v2, domino.player, domino.gameObject, index);
     }
